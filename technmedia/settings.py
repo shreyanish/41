32da9c1d54909df01c347c1e7de5c6e4922f0aed
Shrey Anish G <shreyanish@Shreys-MacBook-Pro.local>
2022-09-30 15:24:42 +0530

    security

--- technmedia/settings.py
+++ technmedia/settings.py
@@ -21,7 +21,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = os.environ.get('SECRET_KEY')
+SECRET_KEY = 'django-insecure-@^9c+1o^xwu#xowv*0u=)__etp5d#fra--gwt5wi(%btzqe=oe'
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
